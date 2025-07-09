ad1ba72af735d22a70d0b0b103b9f35c4ff8a826
Lucas <12496191+lucashuy@users.noreply.github.com>
2025-07-08 21:39:00 -0700

    Added Select Shooting Supplies

--- src/utils/apiStructs.tsx
+++ src/utils/apiStructs.tsx
@@ -120,6 +120,14 @@ export class RetailerEnum {
 		"#000"
 	)
 
+	static readonly SelectShootingSupplies = new Retailer(
+		"Select Shooting Supplies",
+		"https://selectshootingsupplies.com/",
+		"https://cdn11.bigcommerce.com/s-wpb0dq6yc5/images/stencil/original/logobc_1534372578__92678.original.png",
+		"#ed1c24",
+		"#fff"
+	)
+
 	private constructor(
 		private readonly key: string,
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
