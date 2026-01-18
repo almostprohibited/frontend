80503bab2a92f7ca942e3804118a085ff582174c
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-01-17 20:32:28 -0800

    Fix price per round taking floor instead of rounding

--- src/utils/format.tsx
+++ src/utils/format.tsx
@@ -1,6 +1,10 @@
 export function centsToHumanString(price: number): string {
-	const dollars = Math.floor(price / 100);
-	const cents = String(Math.floor(price % 100))
+	// only times this should contain a decimal is if
+	// we are displaying cost per round
+	const roundedPrice = Math.round(price);
+
+	const dollars = Math.floor(roundedPrice / 100);
+	const cents = String(roundedPrice % 100)
 		.padStart(2, '0')
 		.padEnd(2, '0');
 
