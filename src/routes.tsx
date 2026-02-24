5a76799bdb5b0f37f00a8fb8c11d92a4fc54382a
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-02-23 20:13:54 -0800

    Add canonical URL to all pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,91 +3,108 @@ import App from './App';
 import { Category, SortOptions } from './utils/apiStructs';
 import { z } from 'zod/mini';
 
+const metaTags = [
+	{
+		title: 'Almost Prohibited - Browse items from your favourite retailers',
+	},
+	{
+		name: 'description',
+		content:
+			"Canada's upcoming aggregator for firearms, parts, and accessories",
+	},
+	{
+		name: 'viewport',
+		content: 'width=device-width, initial-scale=1.0',
+	},
+	{
+		charSet: 'UTF-8',
+	},
+	{
+		name: 'theme-color',
+		content: '#364fc7',
+	},
+	{
+		property: 'og:title',
+		content:
+			'Almost Prohibited - Browse items from your favourite retailers',
+	},
+	{
+		property: 'og:url',
+		content: 'https://almostprohibited.ca',
+	},
+	{
+		property: 'og:description',
+		content:
+			"Canada's upcoming aggregator for firearms, parts, and accessories",
+	},
+	{
+		property: 'og:image',
+		content: 'https://almostprohibited.ca/favicon.svg',
+	},
+	{
+		property: 'og:type',
+		content: 'website',
+	},
+];
+
+const linkTags = [
+	{
+		rel: 'shortcut icon',
+		href: '/favicon.ico',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		href: '/favicon-96x96.png',
+		sizes: '96x96',
+	},
+	{
+		rel: 'icon',
+		type: 'image/svg+xml',
+		href: '/favicon.svg',
+	},
+	{
+		rel: 'apple-touch-icon',
+		href: '/apple-touch-icon.png',
+		sizes: '180x180',
+	},
+	{
+		rel: 'manifest',
+		href: '/site.webmanifest',
+	},
+	// {
+	// 	rel: 'preconnect',
+	// 	href: 'https://fonts.googleapis.com',
+	// },
+	// {
+	// 	rel: 'preconnect',
+	// 	href: 'https://fonts.gstatic.com',
+	// },
+	// {
+	// 	rel: 'stylesheet',
+	// 	href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+	// },
+];
+
 const rootRoute = createRootRoute({
 	component: () => <App />,
-	head: () => ({
-		meta: [
-			{
-				title: 'Almost Prohibited - Browse items from your favourite retailers',
-			},
-			{
-				name: 'description',
-				content:
-					"Canada's upcoming aggregator for firearms, parts, and accessories",
-			},
-			{
-				name: 'viewport',
-				content: 'width=device-width, initial-scale=1.0',
-			},
-			{
-				charSet: 'UTF-8',
-			},
-			{
-				name: 'theme-color',
-				content: '#364fc7',
-			},
-			{
-				property: 'og:title',
-				content:
-					'Almost Prohibited - Browse items from your favourite retailers',
-			},
-			{
-				property: 'og:url',
-				content: 'https://almostprohibited.ca',
-			},
-			{
-				property: 'og:description',
-				content:
-					"Canada's upcoming aggregator for firearms, parts, and accessories",
-			},
-			{
-				property: 'og:image',
-				content: 'https://almostprohibited.ca/favicon.svg',
-			},
-			{
-				property: 'og:type',
-				content: 'website',
-			},
-		],
-		links: [
-			{
-				rel: 'shortcut icon',
-				href: '/favicon.ico',
-			},
-			{
-				rel: 'icon',
-				type: 'image/png',
-				href: '/favicon-96x96.png',
-				sizes: '96x96',
-			},
-			{
-				rel: 'icon',
-				type: 'image/svg+xml',
-				href: '/favicon.svg',
-			},
-			{
-				rel: 'apple-touch-icon',
-				href: '/apple-touch-icon.png',
-				sizes: '180x180',
-			},
-			{
-				rel: 'manifest',
-				href: '/site.webmanifest',
-			},
-			// {
-			// 	rel: 'preconnect',
-			// 	href: 'https://fonts.googleapis.com',
-			// },
-			// {
-			// 	rel: 'preconnect',
-			// 	href: 'https://fonts.gstatic.com',
-			// },
-			// {
-			// 	rel: 'stylesheet',
-			// 	href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
-			// },
-		],
-	}),
+	head: (context) => {
+		const modifiedLinks = [...linkTags];
+
+		const routeMatch = context.matches.at(-1);
+
+		if (routeMatch) {
+			modifiedLinks.push({
+				rel: 'canonical',
+				href: `https://almostprohibited.ca${routeMatch.pathname}`,
+			});
+		}
+
+		return {
+			meta: metaTags,
+			links: modifiedLinks,
+		};
+	},
 });
 
 const indexRoute = createRoute({
