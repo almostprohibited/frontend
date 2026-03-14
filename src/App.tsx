ad824bb7b9643d61e187767696c575bc291f96da
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-13 22:03:16 -0700

    Update beta warning to be less intrusive

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import {
 	Affix,
-	Alert,
 	Box,
 	ColorSchemeScript,
 	createTheme,
 	Flex,
 	MantineProvider,
-	Stack,
-	Text,
+	Overlay,
 } from '@mantine/core';
-import { IconFlask } from '@tabler/icons-react';
 import Footer from './components/footer/component';
 
 import { useIsBeta } from './utils/hooks/useIsBeta';
@@ -34,29 +31,8 @@ export default function App() {
 			<ColorSchemeScript />
 			<MantineProvider defaultColorScheme="dark" theme={theme}>
 				{isBeta && (
-					<Affix position={{ bottom: 20, right: 20 }}>
-						<a href="https://almostprohibited.ca">
-							<Alert
-								variant="filled"
-								color="grape"
-								title="This is the beta website!"
-								icon={<IconFlask />}
-								maw="20rem"
-							>
-								<Stack>
-									<Text size="sm">
-										{
-											'This site may contain features that are broken, or work in progress.'
-										}
-									</Text>
-									<Text size="sm">
-										{
-											'Click to head back to the release site.'
-										}
-									</Text>
-								</Stack>
-							</Alert>
-						</a>
+					<Affix w="100%" h="100%">
+						<Overlay backgroundOpacity={0} bd="2px solid yellow" />
 					</Affix>
 				)}
 				<Flex direction="column" h="100%">
--- src/components/header/component.tsx
+++ src/components/header/component.tsx
@@ -1,35 +1,46 @@
-import { Box, Flex, Tooltip } from '@mantine/core';
+import { Box, Flex, Text, Tooltip } from '@mantine/core';
 import styles from './component.module.css';
 import { IconBrandGithub, IconHome } from '@tabler/icons-react';
 import { Link } from '@tanstack/react-router';
 import { useMobileView } from '@/utils/hooks/useMobileView';
+import { useIsBeta } from '@/utils/hooks/useIsBeta';
 
 export default function Header() {
+	const isBeta = useIsBeta();
 	const isMobile = useMobileView();
 
 	const iconSize = isMobile ? '1.7rem' : '2rem';
 
-	// Don't think I need the extra flex here
 	return (
-		<Flex className={styles.header} align="center" justify="center">
-			<Box w="100%">
-				<Tooltip label="Home">
-					<Link to="/">
-						<IconHome size={iconSize} />
-					</Link>
-				</Tooltip>
-			</Box>
-			<Box>
-				<Tooltip label="Github source code">
-					<a
-						href="https://github.com/almostprohibited"
-						target="_blank"
-						referrerPolicy="no-referrer"
-					>
-						<IconBrandGithub size={iconSize} />
-					</a>
-				</Tooltip>
-			</Box>
-		</Flex>
+		<>
+			{isBeta && (
+				<Box p="0.5rem" bg="yellow">
+					<Text fw={700} size="lg" ta="center" c="black">
+						This is the beta website! Features may be incomplete, or
+						broken!
+					</Text>
+				</Box>
+			)}
+			<Flex className={styles.header} align="center" justify="center">
+				<Box w="100%">
+					<Tooltip label="Home">
+						<Link to="/">
+							<IconHome size={iconSize} />
+						</Link>
+					</Tooltip>
+				</Box>
+				<Box>
+					<Tooltip label="Github source code">
+						<a
+							href="https://github.com/almostprohibited"
+							target="_blank"
+							referrerPolicy="no-referrer"
+						>
+							<IconBrandGithub size={iconSize} />
+						</a>
+					</Tooltip>
+				</Box>
+			</Flex>
+		</>
 	);
 }
