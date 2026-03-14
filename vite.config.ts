058187cf5c8520ee96990b4db51fad4dea0273ba
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-14 00:57:12 -0700

    Preload dynamic chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
+import preload from 'vite-plugin-preload';
 
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [viteReact()],
+	plugins: [viteReact(), preload()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
