a3639c88ec5cff3100663cb63399cef299d3de74
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-13 22:37:27 -0700

    Remove beta border

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import {
-	Affix,
 	Box,
 	ColorSchemeScript,
 	createTheme,
 	Flex,
 	MantineProvider,
-	Overlay,
 } from '@mantine/core';
 import Footer from './components/footer/component';
 
-import { useIsBeta } from './utils/hooks/useIsBeta';
 import { HeadContent, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import Header from './components/header/component';
@@ -19,8 +16,6 @@ import '@mantine/charts/styles.css';
 import './globals.css';
 
 export default function App() {
-	const isBeta = useIsBeta();
-
 	const theme = createTheme({
 		fontFamily: 'Arial, Helvetica, sans-serif',
 	});
@@ -30,11 +25,6 @@ export default function App() {
 			<HeadContent />
 			<ColorSchemeScript />
 			<MantineProvider defaultColorScheme="dark" theme={theme}>
-				{isBeta && (
-					<Affix w="100%" h="100%">
-						<Overlay backgroundOpacity={0} bd="2px solid yellow" />
-					</Affix>
-				)}
 				<Flex direction="column" h="100%">
 					<Header />
 					<Box mb="2rem" flex={1}>
