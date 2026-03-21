bf5f5b6d03e3d6ac96d8da8f8e0687ef004a87e5
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-21 00:13:33 -0700

    Update footer format and links

--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -1,6 +1,7 @@
 import { Divider, Flex, useMantineTheme } from '@mantine/core';
 import styles from './component.module.css';
 import { Link } from '@tanstack/react-router';
+import { IconBrandGithub } from '@tabler/icons-react';
 
 export default function Footer() {
 	const theme = useMantineTheme();
@@ -12,12 +13,30 @@ export default function Footer() {
 				className={styles.footer}
 				bg={theme.colors.dark[8]}
 				color={theme.colors.dark[1]}
-				justify={'center'}
-				align={'center'}
 				gap={'xl'}
 			>
-				<Link to="/contact/">{'[ Contact ]'}</Link>
-				<Link to="/privacy/">{'[ Privacy Policy ]'}</Link>
+				<Flex direction="column" w="100%" gap="md">
+					<Link to="/contact/">{'[ Contact ]'}</Link>
+					<Link to="/privacy/">{'[ Privacy Policy ]'}</Link>
+				</Flex>
+				<Flex direction="column" w="100%" gap="md" align="end">
+					<a
+						href="https://buymeacoffee.com/almostprohibited"
+						target="_blank"
+						referrerPolicy="no-referrer"
+					>
+						{'[ ☕ Buy me a coffee ]'}
+					</a>
+					<a
+						href="https://github.com/almostprohibited"
+						target="_blank"
+						referrerPolicy="no-referrer"
+					>
+						{'[ '}
+						<IconBrandGithub size="1.1rem" />
+						{' Source code ]'}
+					</a>
+				</Flex>
 			</Flex>
 		</>
 	);
