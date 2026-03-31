e335de82f5d4fe45b4585f6cdf868a85edca02bf
Lucas <12496191+lucashuy@users.noreply.github.com>
2026-03-30 23:57:16 -0700

    Add logout button

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { useAuth } from '@/auth';
+import { getApiDomain } from '@/utils/environment';
 import { Box, Button } from '@mantine/core';
 import { createLazyRoute, useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
 
 export const dashboardLazyRoute = createLazyRoute('/dashboard')({
 	component: DashboardPage,
@@ -10,16 +12,36 @@ function DashboardPage() {
 	const auth = useAuth();
 	const navigate = useNavigate();
 
-	function logout() {
-		auth.logout();
+	const [isLoading, setIsLoading] = useState(false);
 
-		navigate({ to: '/' });
+	function logout() {
+		setIsLoading(true);
+
+		fetch(`${getApiDomain()}/api/auth/logout`, {
+			method: 'DELETE',
+			credentials:
+				process.env.NODE_ENV === 'development'
+					? 'include'
+					: 'same-origin',
+		}).then((response) => {
+			if (response.ok) {
+				auth.logout();
+
+				navigate({ to: '/' });
+			}
+		});
 	}
 
 	return (
 		<>
 			<Box>hello world</Box>
-			<Button onClick={logout}>logout</Button>
+			<Button
+				onClick={logout}
+				loading={isLoading}
+				loaderProps={{ type: 'oval' }}
+			>
+				logout
+			</Button>
 		</>
 	);
 }
